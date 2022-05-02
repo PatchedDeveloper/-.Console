57f79a9897fe9e76e3385b20b8bf0db8a2efdc7b
Trust <103842703+kiddingtrust@users.noreply.github.com>
2022-05-02 16:19:55 +0400

    Add files via upload

---
+++ 2504221/1lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace _1lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            string x;
+            int sqr_x,forx,vx;
+            Console.WriteLine("Введите x");
+            x = Console.ReadLine();
+            int i = Convert.ToInt32(x);
+            sqr_x = (i * i); // 2 step
+            forx = sqr_x * sqr_x; // 4 step
+            vx = forx * forx; // 8 step
+            Console.WriteLine(vx);
+
+
+
+
+        }
+    }
+}
