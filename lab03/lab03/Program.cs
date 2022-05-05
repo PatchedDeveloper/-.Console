08dad0d3b70f70b55c662de35db92de10ab0659f
Trust <103842703+kiddingtrust@users.noreply.github.com>
2022-05-05 20:37:28 +0400

    Add files via upload

---
+++ lab03/lab03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x,f;
+            int a, b, c;
+            Console.WriteLine("Введите значения x (a,b,c- дейтвительные числа)");
+
+            Console.WriteLine("Введите значения x ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значения a ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значения b ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значения c ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if((a<0) && (c!=0))
+            {
+                f = a * Math.Pow(x, 2) + b * x + c;
+                Console.WriteLine("f= " + f);
+            }
+            else if ((a > 0) && (b == 0))
+            {
+                f = -(a) / (x - c); 
+                Console.WriteLine("f= " + f);
+            }
+            else
+            {
+               f= a * (x + c);
+                Console.WriteLine("f= " + f);
+            }
+
+        }
+    }
+}
