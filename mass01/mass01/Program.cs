149ed565635ba01cc336ef0d6c2ad7b419a061be
Trust <103842703+kiddingtrust@users.noreply.github.com>
2022-05-08 02:45:16 +0400

    Add files via upload

---
+++ mass01/mass01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace mass01
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            int x;
+            Console.WriteLine("Введите число n :");
+            x = Convert.ToInt32(Console.ReadLine());
+            SumOfDigits(x);
+        }
+        static void SumOfDigits(int x){
+
+            Console.WriteLine("SumOfDigits Запущен идет рассчет...");
+
+            double one,two,three;
+
+            one = x / 100;
+            two = (x / 10) % 10;
+            three = x % 10;
+
+            double sum = one + two + three;
+
+            Console.WriteLine("Сумма равна "+sum);
+        }
+    }
+}
---
+++ zadacha_2/zadacha_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace zadacha_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int first, two, three;
+            Console.WriteLine("Введите 3 значное число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            first = n / 100;
+            two = (n / 10) % 10;
+            three = n % 10;
+
+            if (n%2==0) {
+                double sum = first + two + three;
+                Console.WriteLine("Ваше число четное его сумма =: "+ sum);
+
+            }
+            else
+            {
+                double proizv = first * two * three;
+                Console.WriteLine("Ваше число не четное его произведение =: " + proizv);
+            }
+        }
+    }
+}
