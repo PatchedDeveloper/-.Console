a826e633d4a1b0ac9a55d20dbf2817c45c2eacab
Trust <103842703+kiddingtrust@users.noreply.github.com>
2022-05-07 19:42:32 +0400

    Add files via upload

---
+++ lab04_3/lab04_3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace lab04_3
+{
+    class Program
+    {
+        static int sum = 0;
+        static int i = 0;
+        static void Main()
+        {
+            int x;
+            Console.WriteLine("");
+            Console.WriteLine("Введите число");
+            x = Convert.ToInt32(Console.ReadLine());
+            NumberOfOdds(x);
+        }
+        static void NumberOfOdds(int x)
+        {
+            Console.WriteLine("NumberOfOdds loaded...");
+
+            double one, two, three;
+
+            one = x / 100;
+            two = (x / 10) % 10;
+            three = x % 10;
+
+            if(i <= 1000000)
+            {
+                if (x > 0 || x < 0)
+                {
+                    if (one % 2 != 0)
+                    {
+                        sum++;
+                    }
+                    if (two % 2 != 0)
+                    {
+                        sum++;
+                    }
+                    if (three % 2 != 0)
+                    {
+                        sum++;
+                    }
+                    Main();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели 0,идёт завершение программы...");
+                    Console.WriteLine("Колличество нечётных цифр " + sum);
+                   
+                }
+                i++;
+             
+            }
+            else
+            {
+                Console.WriteLine("Вы превысили колличество возможных циклов!");
+
+            }
+          
+
+        }
+    }
+}
---
+++ lab4_2/lab4_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab4_2
+{
+    class Program
+    {
+        static int w=0;
+        static void Main()
+        {
+
+ 
+            int x;
+            Console.WriteLine("");
+            Console.WriteLine("Введите число :");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if ( x< 0 || x>0) {
+                w += x;
+                SumOfDigits(x);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели 0, Программа завершена" );
+                Console.WriteLine("Сумма всех чисел равна = "+w);
+            }
+
+        }
+        static void SumOfDigits(int x)
+        {
+            Console.WriteLine("SumOfDigits Запущен идет рассчет...");
+            Console.WriteLine("Ваше число " + x);
+            Main();
+
+        }
+    }
+}
